7d1f407e3a2ddacc5027e931c95de82d80c842d8
kushida.tomohiro <falsity_0330@yahoo.co.jp>
2023-06-01 19:24:51 +0900

    ユーザー一覧　N＋1　対応

--- laravel-practis/app/Http/Controllers/UserController.php
+++ laravel-practis/app/Http/Controllers/UserController.php
@@ -4,13 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Contracts\View\View;
-use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function index(): View
     {
-        $users = User::paginate(10);
+        $users = User::with(['company', 'sections'])->paginate(10);
 
         return view('users.index', compact('users'));
     }
