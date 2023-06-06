b0be9f1bf18b0d3f03d4424c44039f8e18365a79
kushida.tomohiro <falsity_0330@yahoo.co.jp>
2023-06-06 17:25:03 +0900

    CsvExportRecordControllerTest　不要箇所削除

--- laravel-practis/tests/Feature/Http/Controllers/CsvExportRecordControllerTest.php
+++ laravel-practis/tests/Feature/Http/Controllers/CsvExportRecordControllerTest.php
@@ -4,7 +4,6 @@ namespace Tests\Feature\Http\Controllers;
 
 use App\Models\Company;
 use App\Models\User;
-use App\Models\CsvExportRecord;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Storage;
