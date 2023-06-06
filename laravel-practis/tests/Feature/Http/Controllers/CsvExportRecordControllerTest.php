f30b258b610c0efae81ea4a250d4eecbd56d74e1
kushida.tomohiro <falsity_0330@yahoo.co.jp>
2023-06-06 16:53:28 +0900

    CsvExportRecordControllerTest　ダウンロードテスト

--- laravel-practis/tests/Feature/Http/Controllers/CsvExportRecordControllerTest.php
+++ laravel-practis/tests/Feature/Http/Controllers/CsvExportRecordControllerTest.php
@@ -4,8 +4,10 @@ namespace Tests\Feature\Http\Controllers;
 
 use App\Models\Company;
 use App\Models\User;
+use App\Models\CsvExportRecord;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
 class CsvExportRecordControllerTest extends TestCase
@@ -49,18 +51,31 @@ class CsvExportRecordControllerTest extends TestCase
         ]);
     }
 
+    public function test_download()
+    {
+        $url = route('users.csv-export-records.store');
 
-    public function test_download(){
+        $this->get($url)->assertRedirect(route('login'));
+
+        $requestData = [
+            'search' => '', // 検索クエリを空にする
+            'search_option' => 'user', // ユーザー名を検索オプションとして選択
+        ];
+
+        // リクエストを送信
+        $response = $this->actingAs($this->user)->post($url, $requestData);
+
         //CSVファイルのダウンロードが正常に働く
-        //CSVダウンロードボタンをクリックした場合、指定のファイル名でダウンロードされていることを確認する。
+        // レスポンスを検証
+        $response->assertStatus(200);
+        $response->assertDownload();
 
-        //空の検索クエリの場合:
-        //検索クエリが空の場合、すべてのユーザーがCSVファイルに含まれることを確認する。
+        //指定のディレクトリにCSVがダウンロードされている
+        $file_name = 'users-' . now()->format('YmdHis') . '.csv';
+        $file_path = 'csv/' . $file_name;
+        $this->assertTrue(Storage::disk('local')->exists($file_path));
 
-        //ダウンロードしたCSVファイルを確認し、内容が予想どおりであることを確認する
-
-        //ユーザー名で検索した場合、一致するユーザーがCSVファイルに含まれることを確認する。
-        //会社名で検索した場合、一致する会社のみがCSVファイルに含まれることを確認する。
-        //部署名で検索した場合、一致する部署のみがCSVファイルに含まれることを確認する。
+        //指定のファイル名でCSVダウンロードされている
+        $response->assertHeader('content-disposition', 'attachment; filename=' . $file_name);
     }
 }
