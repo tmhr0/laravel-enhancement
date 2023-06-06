bc9b1b02e160fcc6ffc8787bb08cd953080579fe
kushida.tomohiro <falsity_0330@yahoo.co.jp>
2023-06-06 12:34:03 +0900

    CsvExportRecordControllerTest テスト条件　コメント

--- laravel-practis/tests/Feature/Http/Controllers/CsvExportRecordControllerTest.php
+++ laravel-practis/tests/Feature/Http/Controllers/CsvExportRecordControllerTest.php
@@ -31,4 +31,30 @@ class CsvExportRecordControllerTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    public function test_store()
+    {
+        $url = route('users.csv-export-records.store');
+
+        $this->get($url)->assertRedirect(route('login'));
+
+        $response = $this->actingAs($this->user)->get($url);
+
+        $response->assertStatus(200);
+    }
+
+
+    public function test_download(){
+        //CSVファイルのダウンロードが正常に働く
+        //CSVダウンロードボタンをクリックした場合、指定のファイル名でダウンロードされていることを確認する。
+
+        //空の検索クエリの場合:
+        //検索クエリが空の場合、すべてのユーザーがCSVファイルに含まれることを確認する。
+
+        //ダウンロードしたCSVファイルを確認し、内容が予想どおりであることを確認する
+
+        //ユーザー名で検索した場合、一致するユーザーがCSVファイルに含まれることを確認する。
+        //会社名で検索した場合、一致する会社のみがCSVファイルに含まれることを確認する。
+        //部署名で検索した場合、一致する部署のみがCSVファイルに含まれることを確認する。
+    }
 }
