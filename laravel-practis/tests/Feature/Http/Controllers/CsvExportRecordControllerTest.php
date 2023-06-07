3118ae74f9dc79478288fa27ab5855d09b9c052d
kushida.tomohiro <falsity_0330@yahoo.co.jp>
2023-06-07 10:28:04 +0900

    WIP　カバレッジ用　モデルテストの追加

--- laravel-practis/tests/Feature/Http/Controllers/CsvExportRecordControllerTest.php
+++ laravel-practis/tests/Feature/Http/Controllers/CsvExportRecordControllerTest.php
@@ -3,7 +3,10 @@
 namespace Tests\Feature\Http\Controllers;
 
 use App\Models\Company;
+use App\Models\CsvExportRecord;
+use App\Models\Section;
 use App\Models\User;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Storage;
@@ -18,12 +21,85 @@ class CsvExportRecordControllerTest extends TestCase
     {
         parent::setUp();
 
-        $this->company = Company::factory()->create();
-        $this->user = User::factory(['company_id' => $this->company->id])->create();
+        $this->company = Company::factory()->create([
+            'name' => 'アドミン会社'
+        ]);
+
+        $this->user = User::factory([
+            'company_id' => $this->company->id,
+            'name' => 'サンプルアドミン',
+            'role' => 'admin',
+        ])->create();
+
+        $this->section = Section::factory([
+            'company_id' => $this->company->id,
+            'name' => 'アドミン部署'
+        ])->create();
+
+        $this->section->users()->attach($this->user->id);
     }
+
+    public function testUserRelation()
+    {
+        // テスト用のCsvExportRecordインスタンスを作成
+        $csvExportRecord = CsvExportRecord::factory()->create();
+
+        // 関連するUserモデルを取得
+        $user = $csvExportRecord->user;
+
+        // 関連するUserモデルが正しく取得できたかをアサーション
+        $this->assertInstanceOf(User::class, $user);
+        $this->assertEquals($csvExportRecord->download_user_id, $user->id);
+    }
+
+    public function testModelAttributes()
+    {
+        // テスト用のデータを用意
+        $attributes = [
+            'download_user_id' => 1,
+            'file_name' => 'test.csv',
+            'file_path' => '/path/to/file.csv',
+        ];
+
+        // CsvExportRecordインスタンスを作成
+        $csvExportRecord = new CsvExportRecord($attributes);
+
+        // 属性が正しく設定されているかをアサーション
+        $this->assertEquals($attributes['download_user_id'], $csvExportRecord->download_user_id);
+        $this->assertEquals($attributes['file_name'], $csvExportRecord->file_name);
+        $this->assertEquals($attributes['file_path'], $csvExportRecord->file_path);
+    }
+
+    public function testCsvExportRecordUserRelationship()
+    {
+        $user = User::factory()->create();
+
+        $record = CsvExportRecord::factory()->create([
+            'download_user_id' => $user->id,
+        ]);
+
+        $this->assertInstanceOf(User::class, $record->user);
+        $this->assertEquals($user->id, $record->user->id);
+    }
+
+    public function testCsvExportRecordForeignKeyConstraint()
+    {
+        $this->expectException(QueryException::class);
+
+        $recordData = [
+            'download_user_id' => 999, // 存在しないユーザーID
+            'file_name' => 'test.csv',
+        ];
+
+        CsvExportRecord::create($recordData);
+    }
+
 
     public function test_index()
     {
+        // テスト用のダミーデータを作成
+        CsvExportRecord::factory()->count(3)->create();
+
         $url = route('users.csv-export-records.index');
 
         $this->get($url)->assertRedirect(route('login'));
